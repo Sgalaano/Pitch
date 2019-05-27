d02e351c6e1f15f2b718a130397267230326d20b
sgalaano <sgalaano@gmail.com>
2019-05-27 10:48:15 +0300

    added all files

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = 'Galaano1998'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://galaano:1234@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'One Minute Pitch'
+    SENDER_EMAIL = 'sgalaano@gmail.com'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://galaano:1234@localhost/pitch_test'
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://galaano:1234@localhost/pitch'
+
+
+    DEBUG = True
+
+config_options = {
+       'development':DevConfig,
+       'production':ProdConfig,
+       'test':TestConfig
+}
